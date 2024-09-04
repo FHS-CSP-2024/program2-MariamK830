d8a0bc4007c75eb42a87de325eb57a928d383c68
Mariam Khan <mariam.m.khan09@gmail.com>
2024-09-04 14:56:35 +0000

    Finished Notes

---
+++ Demo.py
@@ -0,0 +1,14 @@
+name = input("pelase tell me your name!")
+print("Hi "+ name)
+print(name + "Is a pretty cool name")
+print("I wish my name was" + name)
+print("Hi" +name)
+
+
+
+name = input("Tell me your name now")
+email = input ("Let me send you spam")
+nickname = input("what do your friends call you")
+
+print("Hey" + name + ", or should i say "+ nickname + "." )
+print("I am going to send you a bunch of spam at" +email)  
